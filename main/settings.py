73d5e502185ec5fa55eddc264b72c7d06bd3d093
Izapelini <ivanilsonz@gmail.com>
2016-06-27 15:01:22 -0300

    Adicionado a lib dj-database-url

--- main/settings.py
+++ main/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import dj_database_url
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -76,10 +78,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL', 'sqlite:///{0}'
+                                                             .format(os.path.join(BASE_DIR, 'db.sqlite3')))),
 }
 
 
