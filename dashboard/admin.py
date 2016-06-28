2ba720abc60bec1dc4ac09be86483c28b9ffb9a7
izapelini <ivanilsonz@gmail.com>
2016-06-28 19:02:30 -0300

    Migration 2 incluindo um novo modelo.

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from dashboard.models import Client
+from dashboard.models import Client, Vehicle
 
 
 class ClientAdmin(admin.ModelAdmin):
     pass
 
+
+class VehicleAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Client, ClientAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
--- dashboard/models.py
+++ dashboard/models.py
@@ -9,3 +9,10 @@ class Client(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField('nome', max_length=100, null=False)
     email = models.EmailField('e-mail')
+
+
+class Vehicle(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('nome', max_length=100, null=False)
+    model = models.CharField('model', max_length=100, null=False)
+    year = models.CharField('model', max_length=4)
