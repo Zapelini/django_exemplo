1f0f5c31828c03f07856f1986e0feef548241a52
izapelini <ivanilsonz@gmail.com>
2016-06-28 18:50:18 -0300

    Migration inicial.

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Client(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('nome', max_length=100, null=False)
+    email = models.EmailField('e-mail')
